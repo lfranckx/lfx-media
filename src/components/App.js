d30dc226ee1b1191024d71f7720cbb47157b789f
Bowie <lfranckx@thinkful.com>
2021-08-23 12:50:19 -0500

    remove starter files

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../styles/App.css';
+import { withRouter, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <h1>LFX Media</h1>
+    </>
+  );
+}
+
+export default App;
