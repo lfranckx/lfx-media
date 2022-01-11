bfdbfb067d939c5928efe9ad26798a3bb61f8926
Bowie <lfranckx@thinkful.com>
2022-01-11 17:56:52 -0600

    change logo directory

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/Navbar.scss';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+import green from '../images/green-logo.png';
 
 export default function Navbar(props) {
     const location = useLocation();
@@ -11,7 +12,7 @@ export default function Navbar(props) {
         <div id={props.id} className='nav'>
             <nav>
                 <Link to="/">
-                    <h1><img src='../images/green-logo.png' alt='lfx media logo'/></h1>
+                    <h1><img src={green} alt='lfx media logo'/></h1>
                 </Link>
 
                 <Link className="btn" to="/contact">Say Hello</Link>
