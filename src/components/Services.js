973f1e877380f0e4c7c9a41b08dcd5bb7d5cd9bf
Bowie <lfranckx@thinkful.com>
2022-01-20 14:52:14 -0600

    fix heights to match for service cards

--- src/components/Services.js
+++ src/components/Services.js
@@ -12,7 +12,10 @@ export default function Services() {
                             <i className="fas fa-pencil-alt"></i>
                             <h2>Design</h2>
                             <p>My approach to website design is to create a website that strengthens your company’s brand while ensuring ease of use for your audience.</p>
-                            <button>Learn More <i class="fas fa-chevron-circle-right"></i></button>
+
+                            <div className='btn-wrap'>
+                                <button>Learn More <i class="fas fa-chevron-circle-right"></i></button>
+                            </div>
                         </div>
                     </li>
                     <li>
@@ -20,7 +23,9 @@ export default function Services() {
                             <i className="fas fa-laptop-code"></i>
                             <h2>Development</h2>
                             <p>After creating the design I will start the development phase of your website. We will decide the best platform to build and host your website on depending on your needs and budget.</p>
-                            <button>Learn More <i class="fas fa-chevron-circle-right"></i></button>
+                            <div className='btn-wrap'>
+                                <button>Learn More <i class="fas fa-chevron-circle-right"></i></button>
+                            </div>
                         </div>
                     </li>
                     <li>
@@ -28,10 +33,16 @@ export default function Services() {
                             <i className="fas fa-search-dollar"></i>
                             <h2>SEO</h2>
                             <p>Search Engine Optimization is the practice of improving and promoting a website in order to increase the number of visitors through search engines such as Google.</p>
-                            <button>Learn More <i class="fas fa-chevron-circle-right"></i></button>
+                            <div className='btn-wrap'>
+                                <button>Learn More <i class="fas fa-chevron-circle-right"></i></button>
+                            </div>
                         </div>
                     </li>
                 </ul>
+
+                <div className={'popup'}>
+
+                </div>
             </section>
         </>
     )
