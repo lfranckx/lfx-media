c0342aa4fa50d17287d29ebfa7522c0ecb259a76
Bowie <lfranckx@thinkful.com>
2022-01-22 13:38:07 -0600

    finish pop up window with mobile responsive styling

--- src/components/Services.js
+++ src/components/Services.js
@@ -18,9 +18,9 @@ export default function Services() {
                             <h2>Design</h2>
                             <p>My approach to website design is to create a website that strengthens your company’s brand while ensuring ease of use for your audience.</p>
 
-                            {/* <div className='btn-wrap'>
+                            <div className='btn-wrap'>
                                 <button onClick={() => toggleDesignCard(!designCardOpen)}>Learn More <i className="fas fa-chevron-circle-right"></i></button>
-                            </div> */}
+                            </div>
                         </div>
                     </li>
                     <li>
@@ -28,9 +28,9 @@ export default function Services() {
                             <i className="fas fa-laptop-code"></i>
                             <h2>Development</h2>
                             <p>After creating the design I will start the development phase of your website. We will decide the best platform to build and host your website on depending on your needs and budget.</p>
-                            {/* <div className='btn-wrap'>
+                            <div className='btn-wrap'>
                                 <button onClick={() => {toggleDevelopmentCard(!developmentCardOpen)}}>Learn More <i className="fas fa-chevron-circle-right"></i></button>
-                            </div> */}
+                            </div>
                         </div>
                     </li>
                     <li>
@@ -38,18 +38,21 @@ export default function Services() {
                             <i className="fas fa-search-dollar"></i>
                             <h2>SEO</h2>
                             <p>Search Engine Optimization is the practice of improving and promoting a website in order to increase the number of visitors through search engines such as Google.</p>
-                            {/* <div className='btn-wrap'>
+                            <div className='btn-wrap'>
                                 <button onClick={() => {toggleSeoCard(!seoCardOpen)}}>Learn More <i className="fas fa-chevron-circle-right"></i></button>
-                            </div> */}
+                            </div>
                         </div>
                     </li>
                 </ul>
 
-                {/* <ServicesPopup
+                <ServicesPopup
+                    toggleDesignCard = {toggleDesignCard}
+                    toggleDevelopmentCard = {toggleDevelopmentCard}
+                    toggleSeoCard = {toggleSeoCard}
                     designCardOpen = {designCardOpen}
                     developmentCardOpen = {developmentCardOpen}
                     seoCardOpen = {seoCardOpen}
-                /> */}
+                />
             </section>
         </>
     )
