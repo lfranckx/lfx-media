a7e2ea3d32cbb183a6123690bbc2cedff40996a2
Bowie <lfranckx@thinkful.com>
2022-01-23 19:18:34 -0600

    update navbar contact link to update on location

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../styles/Navbar.scss';
 import { Link } from 'react-router-dom';
-// import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
 import green from '../images/green-logo.png';
 
 export default function Navbar(props) {
-    // const location = useLocation();
+    const location = useLocation();
+    console.log(location.pathname);
 
     return (
         <div id={props.id} className='nav'>
@@ -14,7 +15,8 @@ export default function Navbar(props) {
                     <h1><img src={green} alt='lfx media logo'/></h1>
                 </Link>
 
-                <Link className="btn" to="/contact">Say Hello</Link>
+                {location.pathname === '/' && <Link className="btn" to="/contact">Say Hello</Link>}
+                {location.pathname === '/contact' && <Link className="btn" to="/">Close</Link>}
             </nav>
         </div>
     );
