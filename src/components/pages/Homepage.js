7566e9b593870d29eb474fa611b2e35a30dc999d
Bowie <lfranckx@thinkful.com>
2022-03-31 14:19:49 -0500

    add view my work button to hero

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react';
 import '../../styles/Header.scss';
+import computer from '../../images/computer.png';
+import { Link } from 'react-scroll';
 
 import About from '../About';
 import Projects from '../Projects';
 import Services from '../Services';
 import Testimonials from '../Testimonials';
 import ContactBar from '../ContactBar';
-
-import computer from '../../images/computer.png';
 
 export default function Homepage() {
     useEffect(() => {
@@ -20,6 +20,11 @@ export default function Homepage() {
                 <header>
                     <h1>Web Developer, Full-stack Engineer &amp; SEO Manager</h1>
                     <h2>I build beautiful and effective online stores that help businesses succeed.</h2>
+                    <div className='btn-wrap'>
+                        <Link className='btn' activeClass="active" to="projects" spy={true} smooth={true} offset={0} duration={500}>
+                            View my work <i className="fas fa-chevron-circle-right" />
+                        </Link>
+                    </div>
                     <div className='image-wrap'>
                         <img src={computer} alt='desktop workspace' />
                     </div>
