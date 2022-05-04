d64882da3e9c160a5ffc7c7b31d5d9fb41c35b96
Bowie <lfranckx@thinkful.com>
2022-05-04 10:09:22 -0500

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Navbar.scss';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
-import green from '../images/logo-green.png';
-import white from '../images/logo-white.png';
+import green from '../images/green-logo.png';
 
 export default function Navbar(props) {
     const location = useLocation();
-    const { loading } = props;
+    const [active, setActive] = useState(false);
 
     return (
-        <div className='nav'>
+        <div id={props.id} className='nav'>
             <nav>
                 <Link to="/">
-                    <h1><img src={ loading ? white : green} alt='lfx media logo' className={ loading ? 'loading' : '' } /></h1>
+                    <h1><img src={green} alt='lfx media logo'/></h1>
                 </Link>
 
                 <div className='wrap'>
-                    {location.pathname === '/subscribe' ? <Link className="btn" to="/">Close</Link> : <Link className="btn" to="/subscribe">Newsletter</Link>}
+                    <Link className='navlink' to="/results">Results</Link>
+                    <Link className='navlink' to="/subscribe">Newsletter</Link>
                     {location.pathname === '/contact' ? <Link className="btn" to="/">Close</Link> : <Link className="btn" to="/contact">Say Hello</Link>}
                 </div>
+
+                <div className='toggler' onClick={() => {setActive(!active)}}>
+                    <div className={`hamburger ${active ? 'active' : ''}`}>
+                        <div className='line'></div>
+                    </div>
+                </div>
             </nav>
+            <div id='mobile-menu' className={`${active ? 'show' : 'hide'}`}>
+                <nav className='navlinks'>
+                    <Link className='navlink' to="/results" onClick={() => {setActive(!active)}}>Results</Link>
+                    <Link className='navlink' to="/subscribe" onClick={() => {setActive(!active)}}>Newsletter</Link>
+                    {location.pathname === '/contact' 
+                    ? <Link className="btn" to="/" onClick={() => {setActive(!active)}}>Home</Link> 
+                    : <Link className="btn" to="/contact" onClick={() => {setActive(!active)}}>Say Hello</Link>}
+                </nav>
+            </div>
         </div>
     );
 }
