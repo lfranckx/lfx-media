9ebf50ee8e5610868a353301113039ee934bf4c0
Bowie <lfranckx@thinkful.com>
2022-05-03 22:47:22 -0500

    add hamburger menu for mobile / add results link in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Navbar.scss';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
@@ -6,6 +6,7 @@ import green from '../images/green-logo.png';
 
 export default function Navbar(props) {
     const location = useLocation();
+    const [active, setActive] = useState(false);
 
     return (
         <div id={props.id} className='nav'>
@@ -15,10 +16,24 @@ export default function Navbar(props) {
                 </Link>
 
                 <div className='wrap'>
-                    {location.pathname === '/subscribe' ? <Link className="btn" to="/">Close</Link> : <Link className="btn" to="/subscribe">Newsletter</Link>}
+                    <Link className='navlink' to="/results">Results</Link>
+                    <Link className='navlink' to="/subscribe">Newsletter</Link>
                     {location.pathname === '/contact' ? <Link className="btn" to="/">Close</Link> : <Link className="btn" to="/contact">Say Hello</Link>}
                 </div>
+
+                <div className='toggler' onClick={() => {setActive(!active)}}>
+                    <div className={`hamburger ${active ? 'active' : ''}`}>
+                        <div className='line'></div>
+                    </div>
+                </div>
             </nav>
+            <div id='mobile-menu' className={`${active ? 'show' : 'hide'}`}>
+                <nav className='navlinks'>
+                    <Link className='navlink' to="/results">Results</Link>
+                    <Link className='navlink' to="/subscribe">Newsletter</Link>
+                    {location.pathname === '/contact' ? <Link className="btn" to="/">Close</Link> : <Link className="btn" to="/contact">Say Hello</Link>}
+                </nav>
+            </div>
         </div>
     );
 }
