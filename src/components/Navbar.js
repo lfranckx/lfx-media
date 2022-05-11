346c5121dd7486a67a0a8300faa17504d0d6087b
Bowie <lfranckx@thinkful.com>
2022-05-11 16:32:06 -0500

    update useLocation import

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import '../styles/Navbar.scss';
 import { Link } from 'react-router-dom';
-import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+import { useLocation } from 'react-router-dom';
 import green from '../images/green-logo.png';
 
 export default function Navbar(props) {
